c31d3fad39ab0d8c1e919a77c8d1703375124a2c
myers2004 <spjm926@gmail.com>
2024-01-12 13:06:28 -0800

    entered tests for transcripton: test_transcrbe.py

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -25,11 +25,33 @@ def test_transcribe():
     """
     Write your unit test for the transcribe function here.
     """
-    pass
+    #testing a DNA sequence with allowed nucleotides
+    test_seq = 'ACTGAACCC'
+    test_out = 'UGACUUGGG'
+    assert transcribe(test_seq) == test_out
+
+    #testing a DNA sequence with allowed nucleotides
+    test_seq_error = 'ACYGAACCC'
+    test_error_out = 'A non-allowed nucleotide is present in the sequence.'
+    assert transcribe(test_seq_error) == test_error_out
+    
+    #testing an empty sequence
+    assert transcribe('') == ''
 
 
 def test_reverse_transcribe():
     """
     Write your unit test for the reverse transcribe function here.
     """
-    pass
+    #testing a DNA sequence with allowed nucleotides
+    test_seq = 'ACTGAACCC'
+    test_out = 'GGGUUCAGU'
+    assert transcribe(test_seq) == test_out
+
+    #testing a DNA sequence with allowed nucleotides
+    test_seq_error = 'ACYGAACCC'
+    test_error_out = 'A non-allowed nucleotide is present in the sequence.'
+    assert transcribe(test_seq_error) == test_error_out
+    
+    #testing an empty sequence
+    assert transcribe('') == ''
